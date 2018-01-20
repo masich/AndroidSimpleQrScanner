f71a936b0a1fd2cc57cf2f003321a66ebd37d920
estatico3 <estatico20@gmail.com>
2018-01-20 17:45:24 +0200

    Added QR response handler

--- app/src/main/java/com/simpleqrscanner/MainActivity.kt
+++ app/src/main/java/com/simpleqrscanner/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.simpleqrscanner
 
+import android.app.Activity
+import android.content.Intent
+import android.content.Intent.ACTION_VIEW
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +18,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        onQRScanResult(scanResult)
+    }
+
+    fun onQRScanResult(data: IntentResult?) {
+        if (data == null)
+            finish()
+        else {
+            link.text = data.contents
+            search.setOnClickListener {
+                startActivity(Intent(ACTION_VIEW, Uri.parse(data.contents)))
+            }
+        }
+    }
+
+
 }
